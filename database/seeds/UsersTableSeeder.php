da40926efa71ce2262c7e178fce9d5a26b03205c
sourabhmadhan <52011985+sourabhmadhan@users.noreply.github.com>
2021-06-25 14:12:49 +0530

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,8 +13,8 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'     => "hasib",
-            'email'    => 'hasib@unifiedtransform.com',
+            'name'     => "Sourabh",
+            'email'    => 'Saurabhmadhan@yahoo.com',
             'password' => bcrypt('secret'),
             'role'     => 'master',
             'student_code' => 0000000,
